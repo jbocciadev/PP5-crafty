0da2757da05e1cbb755f5b9264d5004fde17f5dd
jbocciadev <jboccia@gmail.com>
2024-01-10 12:33:04 +0000

    Customise admin.

--- products/admin.py
+++ products/admin.py
@@ -2,7 +2,29 @@ from django.contrib import admin
 from .models import Product, Category, Age_group, Topic
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'age_group',
+        'topic',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Age_group)
 admin.site.register(Topic)
