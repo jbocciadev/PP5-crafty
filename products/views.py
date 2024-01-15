c97206209d05f36fa47e6af76ce5f860fbe3ec4b
jbocciadev <jboccia@gmail.com>
2024-01-15 12:05:45 +0000

    Implement sorting JS and back-to-top arrow button.

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category, Topic, Age_group
 
 # Create your views here.
@@ -25,7 +26,10 @@ def all_products(request):
                 products = products.annotate(lower_name=Lower('name'))
 
             if sortkey == 'category':
-                sortkey = 'category__name'
+                sortkey = 'category__friendly_name'
+            
+            if sortkey == 'age':
+                sortkey = 'age_group'
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
