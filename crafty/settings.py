4154919133c6bb8d83b36c5519aa697c09a6d8df
Juan A. Boccia <jboccia@gmail.com>
2024-01-22 18:46:08 +0000

    Add bag context and delivery charges logic.

--- crafty/settings.py
+++ crafty/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
@@ -147,3 +148,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+FREE_DELIVERY_THRESHOLD = 50
+
+STANDARD_DELIVERY_PERCENTAGE = 10
