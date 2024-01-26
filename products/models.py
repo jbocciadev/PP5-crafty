859744d9d9292a6bd3ce8490f7bddddb9b6e61df
Juan A. Boccia <jboccia@gmail.com>
2024-01-26 11:28:06 +0000

    Add sizes to product model and size selector box to product template.

--- products/models.py
+++ products/models.py
@@ -21,6 +21,7 @@ class Product(models.Model):
     age_group = models.ForeignKey('Age_group', null=True, blank=True, on_delete=models.SET_NULL)
     topic = models.ForeignKey('Topic', null=True, blank=True, on_delete=models.SET_NULL)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
