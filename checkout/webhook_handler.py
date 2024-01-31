2ee81060d341f9c5e582f9bb315742f802d1a20a
jbocciadev <jboccia@gmail.com>
2024-01-31 15:00:58 +0000

    Fix webhooks code.

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 class StripeWH_Handler:
-    """ Handle Stripe webhooks"""
+    """Handle Stripe webhooks"""
 
     def __init__(self, request):
         self.request = request
@@ -17,11 +17,11 @@ class StripeWH_Handler:
 
     def handle_payment_intent_succeeded(self, event):
         """
-        Handle the payment_intent.succeeded webhook from stripe
+        Handle the payment_intent.succeeded webhook from Stripe
         """
         intent = event.data.object
         print(intent)
-        
+
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
-
 from checkout.webhook_handler import StripeWH_Handler
 
 import stripe
@@ -10,12 +9,12 @@ import stripe
 @require_POST
 @csrf_exempt
 def webhook(request):
-    """ Listen for webhooks from Stripe"""
+    """Listen for webhooks from Stripe"""
     # Setup
     wh_secret = settings.STRIPE_WH_SECRET
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
-    #Get the webhook data and verify its signature
+    # Get the webhook data and verify its signature
     payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
@@ -35,13 +34,12 @@ def webhook(request):
     except Exception as e:
         return HttpResponse(content=e, status=400)
 
-    
-    # Setup a webhook handler
+    # Set up a webhook handler
     handler = StripeWH_Handler(request)
 
     # Map webhook events to relevant handler functions
     event_map = {
-        'payment_intent_succeeded': handler.handle_payment_intent_succeeded,
+        'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
         'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
     }
 
@@ -55,4 +53,3 @@ def webhook(request):
     # Call the event handler with the event
     response = event_handler(event)
     return response
-    
