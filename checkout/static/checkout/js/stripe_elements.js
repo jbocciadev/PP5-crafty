dd90f3143a20eae4016b1508bc83d8f82ef90121
jbocciadev <jboccia@gmail.com>
2024-01-31 12:18:40 +0000

    Add Stripe webhook handler and fix spinner css.

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -30,8 +30,6 @@ var style = {
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
-d.mount('#card-element');
-
 // Handle real-time validation errors on the card element
 card.addEventListener('change', function(event){
     var errorDiv = document.getElementById('card-errors');
