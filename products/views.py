121c31367130a7591401364215185bde44d4c954
jbocciadev <jboccia@gmail.com>
2024-02-07 16:11:02 +0000

    Add product edit capability.

--- products/views.py
+++ products/views.py
@@ -36,7 +36,6 @@ def all_products(request):
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
-
             products = products.order_by(sortkey)
 
         if 'q' in request.GET:
@@ -46,9 +45,9 @@ def all_products(request):
                 return redirect (reverse('products'))
 
             queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(category__name__icontains=query)
-            print(queries)
             products = products.filter(queries)
 
+        # Compound queryset to accommodate for multiple-categories
         compound_q = Q()
 
         if 'category' in request.GET:
@@ -93,7 +92,6 @@ def product_detail(request, product_id):
 def add_product(request):
     """ Add a product to the store """
 
-
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -108,6 +106,31 @@ def add_product(request):
     template = 'products/add_product.html'
     context = {
         'form': form,
+    }
+
+    return render(request, template, context)
+
+
+
+def edit_product(request, product_id):
+    """ Edit a product in the store """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
     }
 
     return render(request, template, context)
